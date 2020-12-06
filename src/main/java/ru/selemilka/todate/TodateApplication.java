2861ff9a35b19b34609fa4646daba7edd8201bdf
Fomin Sergey <sdfomin@edu.hse.ru>
2020-12-06 22:16:26 +0300

    Initial commit

---
+++ src/main/java/ru/selemilka/todate/TodateApplication.java
@@ -0,0 +1,13 @@
+package ru.selemilka.todate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/selemilka/todate/TodateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.selemilka.todate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
